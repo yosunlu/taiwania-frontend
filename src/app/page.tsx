2d2b5bf8f88efb3ef75f61e01c1002330c2232a3
yushanlu <yushanlu1031@gmail.com>
2024-08-18 02:09:27 +0800

    Add Taiwania logo and Taiwan icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
+import '@fontsource/playfair-display/600.css';
 
+const styles = {
+  main: {
+    textAlign: 'center',
+    marginTop: '2vh',
+  },
+  title: {
+    fontFamily: "'Playfair Display', serif",
+    fontWeight: 600,
+    fontSize: '4rem', 
+    color: '#004d40',
+    margin: '0',
+  },
+};
 
 export default function Home() {
   return (
-    <main>
-      Home page
+    <main className='flex flex-col mt-2 items-center'>
+      <h1 style={styles.title}>Taiwania</h1>
+      
     </main>
   );
 }
+
--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,6 @@
 
 export default function Container({children} : any) {
   return (
-    <div className="flex flex-col min-h-screen">{children}</div>
+    <div className="flex flex-col min-h-screen max-w-6xl mx-auto">{children}</div>
   )
 }
