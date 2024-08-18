4728224340d4e824a4229b566e7efaa195cf6a62
yushanlu <yushanlu1031@gmail.com>
2024-08-18 20:55:05 +0800

    Convert title to TailWind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,7 @@
-import '@fontsource/playfair-display/600.css';
-
-const styles = {
-  main: {
-    textAlign: 'center',
-    marginTop: '2vh',
-  },
-  title: {
-    fontFamily: "'Playfair Display', serif",
-    fontWeight: 600,
-    fontSize: '4rem', 
-    color: '#004d40',
-    margin: '0',
-  },
-};
-
 export default function Home() {
   return (
     <main className='flex flex-col mt-2 items-center'>
-      <h1 style={styles.title}>Taiwania</h1>
+      <h1 className='font-DM mt-2 text-7xl text-emerald-900 text-bold'>Taiwania</h1>
       
     </main>
   );
